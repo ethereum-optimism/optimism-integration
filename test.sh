0705c69debae1833d757a5d77f3b665ae8b09b02
Karl Floersch <karl@karlfloersch.com>
2020-09-28 00:47:47 -0400

    Fix test.sh to remove volumes

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.local.yml rm;
+docker-compose -f docker-compose.local.yml down -v --remove-orphans;
 
 docker-compose -f docker-compose.local.yml up --exit-code-from integration_tests;
