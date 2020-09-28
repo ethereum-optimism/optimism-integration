d7aa6078ae8ea78cfe7f8075dd707a0532b5d3f1
Mark Tyneway <mark.tyneway@gmail.com>
2020-09-28 13:16:01 -0700

    builds: mount

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-docker-compose -f docker-compose.local.yml rm;
+#!/bin/bash
 
-docker-compose -f docker-compose.local.yml --env-file=docker-compose.build.env up
+SERVICES='geth_l2 postgres l1_chain microservices integration_tests'
+
+GIT_MODULES=$(cat .gitmodules | grep path | cut -d '=' -f2 | tr -d ' ' | tr '\n' ' ')
+echo "Building $GIT_MODULES"
+
+docker-compose -f docker-compose.local.yml build $SERVICES
--- test.sh
+++ test.sh
@@ -1,3 +1,32 @@
-docker-compose -f docker-compose.local.yml down -v --remove-orphans;
+#!/bin/bash
 
-docker-compose -f docker-compose.local.yml up --exit-code-from integration_tests;
+USAGE="
+./test.sh
+
+CLI Arguments:
+  -r|--rebuild    - rebuild the images
+"
+
+REBUILD=""
+
+while (( "$#" )); do
+  case "$1" in
+    -r|--rebuild)
+        REBUILD=1
+        shift
+      ;;
+    -h|--help)
+        echo $USAGE
+        exit 0
+    esac
+done
+
+docker-compose -f docker-compose.local.yml rm -f
+
+sleep 1
+
+REBUILD=$REBUILD \
+    docker-compose -f docker-compose.local.yml \
+        up \
+        --exit-code-from integration_tests \
+        --abort-on-container-exit
