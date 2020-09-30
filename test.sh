7f16edb05a9248bf54c71e40b8d31e30ca69794b
Mark Tyneway <mark.tyneway@gmail.com>
2020-09-29 17:49:22 -0700

    builds: optimize

--- test.sh
+++ test.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
 
-USAGE="
-./test.sh
-
-CLI Arguments:
-  -r|--rebuild    - rebuild the images
-"
-
-REBUILD=""
-
-while (( "$#" )); do
-  case "$1" in
-    -r|--rebuild)
-        REBUILD=1
-        shift
-      ;;
-    -h|--help)
-        echo $USAGE
-        exit 0
-    esac
-done
-
 docker-compose -f docker-compose.local.yml rm -f
 
-sleep 1
+docker volume ls \
+    --filter='label=com.docker.compose.project=optimism-integration' \
+    | xargs docker volume rm 2&>/dev/null
 
-REBUILD=$REBUILD \
-    docker-compose -f docker-compose.local.yml \
-        up \
-        --exit-code-from integration_tests \
-        --abort-on-container-exit
+docker-compose -f docker-compose.local.yml up \
+    --exit-code-from integration_tests \
+    --abort-on-container-exit
