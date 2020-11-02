0297faad12e171ece6f4cd4e540d10ddcda40501
Mark Tyneway <mark.tyneway@gmail.com>
2020-11-02 15:25:20 -0800

    test script: begin update

--- test.sh
+++ test.sh
@@ -4,14 +4,16 @@
 # The name of the directory must match the package name
 # with the "eth-optimism/" prefix.
 
+DOCKERFILE=docker-compose.local.yml
+
 if [ ! -z "$PKGS" ]; then
-    docker-compose -f docker-compose.yml rm -f
+    docker-compose -f $DOCKERFILE  rm -f
     docker volume ls \
         --filter='label=com.docker.compose.project=optimism-integration' \
         | xargs docker volume rm 2&>/dev/null
 
     PKGS=$PKGS \
-        docker-compose -f docker-compose.yml \
+        docker-compose -f $DOCKERFILE \
             up \
             --exit-code-from integration_tests \
             --abort-on-container-exit
@@ -24,13 +26,13 @@ else
         PKGS=$(basename $PACKAGE_PATH)
         echo "Running $PKGS test suite"
 
-        docker-compose -f docker-compose.yml rm -f
+        docker-compose -f $DOCKERFILE rm -f
         docker volume ls \
             --filter='label=com.docker.compose.project=optimism-integration' \
             | xargs docker volume rm 2&>/dev/null
 
         PKGS=$PKGS \
-            docker-compose -f docker-compose.yml \
+            docker-compose -f $DOCKERFILE \
                 --env-file docker-compose.microservices.env \
                 up \
                 --exit-code-from integration_tests \
