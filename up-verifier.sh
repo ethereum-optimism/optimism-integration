3bf51c63fd091270dab1103c78677e71dd39c90f
Mark Tyneway <mark.tyneway@gmail.com>
2020-11-24 10:09:56 -0800

    verifier: up script

--- up-verifier.sh
+++ up-verifier.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 DOCKERFILE='docker-compose.local.yml'
+SERVICES="up deployer verifier l1_chain batch_submitter geth_l2"
+
+while (( "$#" )); do
+  case "$1" in
+    -l|--local)
+      DOCKERFILE="docker-compose.local.yml"
+      shift 1
+      ;;
+    -s|--services)
+      SERVICES="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown argument $1" >&2
+      shift
+      ;;
+  esac
+done
 
 docker-compose \
     -f $DOCKERFILE \
     -f optional/verifier-service.yml \
     -f optional/verifier-service.local.yml \
-    rm -f -v
-
-docker volume ls --format='{{.Name}}' | xargs docker volume rm 2&>/dev/null
+    down -v --remove-orphans
 
 docker-compose \
     -f $DOCKERFILE \
     -f optional/verifier-service.yml \
     -f optional/verifier-service.local.yml \
-    up deployer verifier l1_chain batch_submitter geth_l2
-
+    $SERVICES
