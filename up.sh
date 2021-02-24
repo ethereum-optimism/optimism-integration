4a0b783731f78cfba0043a93d296aba1be2d023b
Mark Tyneway <mark.tyneway@gmail.com>
2021-02-23 16:52:54 -0800

    up: log branch/commit hash when running locally (#47)
    * up: log branch/commit hash when running locally
    
    * up: print branch/commits

--- up.sh
+++ up.sh
@@ -7,11 +7,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 SERVICES='geth_l2 l1_chain batch_submitter deployer message_relayer data_transport_layer'
 DOCKERFILE="docker-compose.yml"
+IS_LOCAL=
 
 while (( "$#" )); do
   case "$1" in
     -l|--local)
       DOCKERFILE="docker-compose.local.yml"
+      IS_LOCAL=true
       shift 1
       ;;
     -s|--services)
@@ -24,6 +26,22 @@ while (( "$#" )); do
       ;;
   esac
 done
+
+
+function branch() {
+    BRANCH=$(git --git-dir $DIR/$1 branch --show-current)
+    COMMIT=$(git --git-dir $DIR/$1 rev-parse --short HEAD)
+    echo "$1 $BRANCH $COMMIT"
+}
+
+if [ ! -z "$IS_LOCAL" ]; then
+    git submodule foreach \
+        '
+            BRANCH=$(git branch --show-current)
+            COMMIT=$(git rev-parse --short HEAD)
+            echo "$BRANCH $COMMIT"
+        '
+fi
 
 docker-compose \
     -f $DIR/$DOCKERFILE \
