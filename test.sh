69bcb6958cf03c7b76a0c35d1250d778ee054fff
Liam Horne <liam@lihorne.com>
2021-03-04 12:36:30 -0500

    test: update test script to emit log files per docker-compose service

--- test.sh
+++ test.sh
@@ -51,6 +51,10 @@ DATA_TRANSPORT_LAYER_TAG=$(echo $DATA_TRANSPORT_LAYER_TAG | sed 's/\//_/g')
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 function run {
+    # Create artifacts folder for logs
+    artifacts_folder="./artifacts/$PKGS"
+    mkdir -p $artifacts_folder
+
     local cmd
     cmd="docker-compose -f $DIR/$DOCKERFILE"
     cmd="$cmd -f $DIR/docker-compose.env.yml"
@@ -60,6 +64,9 @@ function run {
     cmd="$cmd up"
     cmd="$cmd --exit-code-from integration_tests"
     cmd="$cmd --abort-on-container-exit"
+    cmd="$cmd --no-color"
+
+    echo "Logs available per-service at $artifacts_folder"
 
     PKGS=$PKGS \
     DEPLOYER_TAG=$DEPLOYER_TAG \
@@ -69,7 +76,20 @@ function run {
     INTEGRATION_TESTS_TAG=$INTEGRATION_TESTS_TAG \
     MESSAGE_RELAYER_TAG=$MESSAGE_RELAYER_TAG \
     DATA_TRANSPORT_LAYER_TAG=$DATA_TRANSPORT_LAYER_TAG \
-        $cmd
+        $cmd > $artifacts_folder/process.log # Send all process logs to process.log
+
+    # Send all process logs to artifacts folder w/ service name as filename
+    cat $artifacts_folder/process.log | grep -e "|" | \
+      # Delimiter based on | which docker-compose uses in streamed logs
+      awk '{
+        idx = index($0, "| ");                  # Assign index of delimiter between service and log
+        service = substr($0, 0, idx);           # Assign variable for service
+        gsub("[^a-zA-Z0-9_]", "", service);     # Substitute any unnatural characters (e.g., spaces)
+        gsub("$", ".log", service);             # Add .log to end of variable (e.g., l1_chain.log)
+        outputfile = sprintf (service);         # Assign output file name to variable
+        print substr($0, idx + 2) > outputfile; # Send corresponding log to corresponding log file
+      }'
+
 }
 
 function clean {
